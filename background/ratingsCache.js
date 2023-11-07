75c9d7c1e33cda19ac83c5a8eda87463cdbda3ad
Avi Baruch <avineedswifi@gmail.com>
2023-11-07 13:33:49 -0500

    added number of ratings, working on adding grade breakdown

--- background/ratingsCache.js
+++ background/ratingsCache.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        url = `https://planetterp.com/api/v1/professor?name=${request.prof}`
+        url = `https://planetterp.com/api/v1/professor?name=${request.prof}&reviews=true`
 
         caches.match(url).then((response) => {
             expTime = 86400000
@@ -10,8 +10,8 @@ chrome.runtime.onMessage.addListener(
                     sendResponse({error: false, data: json})
                 })
             } else {
-                console.log("fetching for "+request.prof+" @ url "+`https://planetterp.com/api/v1/professor?name=${request.prof}`)
-                fetch(`https://planetterp.com/api/v1/professor?name=${request.prof}`)
+                console.log("fetching for "+request.prof+" @ url "+`https://planetterp.com/api/v1/professor?name=${request.prof}&reviews=true`)
+                fetch(`https://planetterp.com/api/v1/professor?name=${request.prof}&reviews=true`)
                 .then((response) => {
                     let responseClone = response.clone();
         
@@ -28,7 +28,7 @@ chrome.runtime.onMessage.addListener(
                                     headers: headers
                                 }
                             )
-                            cache.put(`https://planetterp.com/api/v1/professor?name=${request.prof}`, newresp);
+                            cache.put(`https://planetterp.com/api/v1/professor?name=${request.prof}&reviews=true`, newresp);
                         })
                     });
                     response.json().then(json => {
@@ -38,54 +38,6 @@ chrome.runtime.onMessage.addListener(
             }
         })
         
-        return true;
-
-        /* 
-        console.log(url)
-        fetch(url).then(response=>{
-            console.log("sending data")
-            sendResponse({error: false, data: response.json()})
-        })
-        .catch(e => sendResponse({error: e.message}));
-        
-        return true;*/
-
-        /*
-        try {
-            let resp = {error: true};
-            sendResponse({})
-            attemptGet = await chrome.storage.local.get([request.prof])
-            if (attemptGet.profName) {
-                console.log(`${request.prof} already recorded!`)
-                resp = attemptGet.profName
-                sendResponse(resp)
-            }
-            else {
-                console.log(`${request.prof} recording!`)
-                x = request.prof
-                data = {error: false, average_rating: "BLA", profName: request.prof}
-                setData = await chrome.storage.local.set({x: data})
-                resp = data
-                console.log(`Recorded: ${resp.profName}`)
-                sendResponse({error: true})
-                console.log("I shouldnt be able to see this message")
-            }
-            sendResponse(resp)
-        } catch (err) {
-            console.log(err)
-            sendResponse({error: true})
-        }
-        */
-
-        /*chrome.storage.local.set({"test": true}).then(()=>{
-            console.log("DONE, doing search")
-            chrome.storage.local.get(["notest"]).then(result=>{
-                console.log("test found!: ", result)
-            })
-
-        })*/
-
-        // sendResponse({error: true, average_rating: "BLA"});
-          
+        return true;        
     }
   );
